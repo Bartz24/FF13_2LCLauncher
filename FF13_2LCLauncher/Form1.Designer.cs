acc98ccb3155f59ded7636a78f57f3b96663d57c
bartz24games@gmail.com <platinumgames347022>
2020-06-15 11:27:14 -0400

    Add project files.

---
+++ FF13_2LCLauncher/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace FF13_2LCLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBoxDisplay = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxResolution = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxVoices = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxShadows = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxMSAA = new System.Windows.Forms.ComboBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxDisplay
+            // 
+            this.comboBoxDisplay.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.comboBoxDisplay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDisplay.FormattingEnabled = true;
+            this.comboBoxDisplay.Items.AddRange(new object[] {
+            "Windowed",
+            "Full Screen"});
+            this.comboBoxDisplay.Location = new System.Drawing.Point(12, 71);
+            this.comboBoxDisplay.Name = "comboBoxDisplay";
+            this.comboBoxDisplay.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDisplay.TabIndex = 0;
+            this.comboBoxDisplay.SelectedIndexChanged += new System.EventHandler(this.comboBoxDisplay_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Display Mode";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Resolution";
+            // 
+            // comboBoxResolution
+            // 
+            this.comboBoxResolution.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.comboBoxResolution.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxResolution.FormattingEnabled = true;
+            this.comboBoxResolution.Items.AddRange(new object[] {
+            "1280 x 720",
+            "1920 x 1080"});
+            this.comboBoxResolution.Location = new System.Drawing.Point(12, 116);
+            this.comboBoxResolution.Name = "comboBoxResolution";
+            this.comboBoxResolution.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxResolution.TabIndex = 2;
+            this.comboBoxResolution.SelectedIndexChanged += new System.EventHandler(this.comboBoxResolution_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Voices";
+            // 
+            // comboBoxVoices
+            // 
+            this.comboBoxVoices.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.comboBoxVoices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxVoices.FormattingEnabled = true;
+            this.comboBoxVoices.Items.AddRange(new object[] {
+            "English",
+            "Japanese"});
+            this.comboBoxVoices.Location = new System.Drawing.Point(12, 163);
+            this.comboBoxVoices.Name = "comboBoxVoices";
+            this.comboBoxVoices.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxVoices.TabIndex = 4;
+            this.comboBoxVoices.SelectedIndexChanged += new System.EventHandler(this.comboBoxVoices_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Shadow Resolution";
+            // 
+            // comboBoxShadows
+            // 
+            this.comboBoxShadows.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.comboBoxShadows.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxShadows.FormattingEnabled = true;
+            this.comboBoxShadows.Items.AddRange(new object[] {
+            "512 x 512",
+            "1024 x 1024",
+            "2048 x 2048",
+            "4096 x 4096",
+            "8192 x 8192"});
+            this.comboBoxShadows.Location = new System.Drawing.Point(12, 210);
+            this.comboBoxShadows.Name = "comboBoxShadows";
+            this.comboBoxShadows.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxShadows.TabIndex = 6;
+            this.comboBoxShadows.SelectedIndexChanged += new System.EventHandler(this.comboBoxShadows_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "MSAA Anti-Aliasing ";
+            // 
+            // comboBoxMSAA
+            // 
+            this.comboBoxMSAA.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.comboBoxMSAA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMSAA.FormattingEnabled = true;
+            this.comboBoxMSAA.Items.AddRange(new object[] {
+            "x2",
+            "x4",
+            "x8",
+            "x16"});
+            this.comboBoxMSAA.Location = new System.Drawing.Point(12, 255);
+            this.comboBoxMSAA.Name = "comboBoxMSAA";
+            this.comboBoxMSAA.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMSAA.TabIndex = 8;
+            this.comboBoxMSAA.SelectedIndexChanged += new System.EventHandler(this.comboBoxMSAA_SelectedIndexChanged);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonStart.Location = new System.Drawing.Point(329, 265);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(116, 23);
+            this.buttonStart.TabIndex = 10;
+            this.buttonStart.Text = "Start Game";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(417, 39);
+            this.label6.TabIndex = 11;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 300);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxMSAA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBoxShadows);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxVoices);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxResolution);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxDisplay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "FFXIII-2 160f Lucky Coin Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxDisplay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxResolution;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxVoices;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxShadows;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxMSAA;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ FF13_2LCLauncher/Form1.cs
@@ -0,0 +1,233 @@
+﻿using memory;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FF13_2LCLauncher
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern int SetWindowText(IntPtr hWnd, string text);
+
+        private static Properties.Settings settings = Properties.Settings.Default;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxDisplay.SelectedIndex = comboBoxDisplay.Items.IndexOf(settings.Display);
+            comboBoxResolution.SelectedIndex = comboBoxResolution.Items.IndexOf(settings.Resolution);
+            comboBoxVoices.SelectedIndex = comboBoxVoices.Items.IndexOf(settings.Voices);
+            comboBoxShadows.SelectedIndex = comboBoxShadows.Items.IndexOf(settings.Shadows);
+            comboBoxMSAA.SelectedIndex = comboBoxMSAA.Items.IndexOf(settings.MSAA);
+
+            AutoSearchDir();
+        }
+
+        private void comboBoxDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            settings.Display = comboBoxDisplay.Text;
+            settings.Save();
+        }
+
+        private void comboBoxResolution_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            settings.Resolution = comboBoxResolution.Text;
+            settings.Save();
+        }
+
+        private void comboBoxVoices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            settings.Voices = comboBoxVoices.Text;
+            settings.Save();
+        }
+
+        private void comboBoxShadows_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            settings.Shadows = comboBoxShadows.Text;
+            settings.Save();
+        }
+
+        private void comboBoxMSAA_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            settings.MSAA = comboBoxMSAA.Text;
+            settings.Save();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            String path = GetFF13Directory();
+            if (String.IsNullOrEmpty(path))
+            {
+                MessageBox.Show("Could not find the FF13-2 steam files! Report this issue to the dev, Bartz24!");
+                return;
+            }
+            Process process = new Process();
+            process.StartInfo.FileName = path + "\\alba_data\\prog\\win\\bin\\ffxiii2img.exe";
+            process.StartInfo.Arguments = String.Join(" ", GetArgs());
+            process.Start();
+
+            Thread.Sleep(5000);
+
+            SetWindowText(process.MainWindowHandle, "FINAL FANTASY XIII-2 160f Lucky Coin Mod");
+
+            Scanner scanner = new Scanner(process, process.Handle, "8A D0 83 C9 FF");
+
+            scanner.setModule(process.MainModule);
+            scanner.writeBytes(process, scanner.FindPattern(), "B2 37 83 C9 FF");
+
+            this.Close();
+        }
+
+        private List<string> GetArgs()
+        {
+            List<string> arguments = new List<string>();
+            switch (comboBoxDisplay.Text)
+            {
+                case "Full Screen":
+                    arguments.Add("-FullScreenMode=Force");
+                    break;
+                case "Windowed":
+                default:
+                    break;
+            }
+
+            switch (comboBoxResolution.Text)
+            {
+                case "1920 x 1080":
+                    arguments.Add("-Width=1920 -Height=1080");
+                    break;
+                case "1280 x 720":
+                default:
+                    arguments.Add("-Width=1280 -Height=720");
+                    break;
+            }
+
+            switch (comboBoxShadows.Text)
+            {
+                case "1024 x 1024":
+                    arguments.Add("-Shadow=1024");
+                    break;
+                case "2048 x 2048":
+                    arguments.Add("-Shadow=2048");
+                    break;
+                case "4096 x 4096":
+                    arguments.Add("-Shadow=4096");
+                    break;
+                case "8192 x 8192":
+                    arguments.Add("-Shadow=8192");
+                    break;
+                case "512 x 512":
+                default:
+                    arguments.Add("-Shadow=512");
+                    break;
+            }
+
+            switch (comboBoxVoices.Text)
+            {
+                case "Japanese":
+                    arguments.Add("-VoiceJPMode");
+                    break;
+                case "English":
+                default:
+                    break;
+            }
+
+            switch (comboBoxMSAA.Text)
+            {
+                case "x4":
+                    arguments.Add("-MSAA=4");
+                    break;
+                case "x8":
+                    arguments.Add("-MSAA=8");
+                    break;
+                case "x16":
+                    arguments.Add("-MSAA=16");
+                    break;
+                case "x2":
+                default:
+                    arguments.Add("-MSAA=2");
+                    break;
+            }
+
+            return arguments;
+        }
+
+        private void AutoSearchDir()
+        {
+            object returnVal = Registry.GetValue("HKEY_CURRENT_USER\\Software\\Valve\\Steam", "SteamPath", "c:/program files (x86)/steam");
+            returnVal = returnVal.ToString().Replace("/", "\\");
+
+            if (GetFF13Path(returnVal.ToString()) == null)
+            {
+                string text = File.ReadAllText(returnVal.ToString() + "/steamapps/libraryfolders.vdf");
+                Regex regex = new Regex("\"\\d+\"\\s+\"(.*)\"");
+                foreach (Match match in regex.Matches(text))
+                {
+                    if (match.Success)
+                    {
+                        if (GetFF13Path(match.Groups[1].Value) != null)
+                            break;
+                    }
+                }
+            }
+        }
+
+        public static string GetFF13Directory()
+        {
+            string path = GetFF13Path()?.Replace("\\\\", "\\");
+            if (!String.IsNullOrEmpty(path) && path.EndsWith("\\FFXiiiSteam.dll"))
+                return path.Substring(0, path.Length - "\\FFXiiiSteam.dll".Length);
+            return null;
+        }
+
+        private static string FF13FilePath = null;
+
+        private static string GetFF13Path(string directoryCheck = null)
+        {
+            if (String.IsNullOrEmpty(FF13FilePath) || !File.Exists(FF13FilePath) || !FF13FilePath.EndsWith("FINAL FANTASY XIII-2\\FFXiiiSteam.dll"))
+            {
+                FF13FilePath = null;
+                if (File.Exists("ff13_2path.txt"))
+                {
+                    FF13FilePath = File.ReadAllText("ff13_2path.txt").Replace("/", "\\");
+                    FF13FilePath = (String.IsNullOrEmpty(FF13FilePath) || !File.Exists(FF13FilePath.Substring(0, FF13FilePath.LastIndexOf("\\")) + "\\alba_data\\prog\\win\\bin\\ffxiii2img.exe")) ? null : FF13FilePath;
+                    if (File.Exists(FF13FilePath))
+                        return FF13FilePath;
+                }
+                if (directoryCheck != null && Directory.Exists(directoryCheck))
+                {
+                    string[] paths = Directory.GetFiles(directoryCheck, "FFXiiiSteam.dll", SearchOption.AllDirectories);
+                    paths.ToList().ForEach(p =>
+                    {
+                        p = p.Replace("/", "\\").Replace("\\\\", "\\");
+                        string path = p.Substring(0, p.LastIndexOf("\\")) + "\\alba_data\\prog\\win\\bin\\ffxiii2img.exe";
+                        if (File.Exists(path))
+                            FF13FilePath = p.Replace("/", "\\");
+                    });
+                }
+            }
+
+            if (FF13FilePath != null)
+                File.WriteAllText("ff13_2path.txt", FF13FilePath);
+            else
+                File.WriteAllText("ff13_2path.txt", "");
+            return FF13FilePath;
+        }
+    }
+}
